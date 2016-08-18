d4cb7b8ed173335ef284ec28cba9da4a4c0ca32e
William Casarin <bill@casarin.me>
2016-08-18 14:55:45 -0700

    flexible-instances: bump to 0.3.0

--- pkgs/haskell/flexible-instances.nix
+++ pkgs/haskell/flexible-instances.nix
@@ -1,16 +1,18 @@
 { mkDerivation, aeson, base, data-default, flexible, mtl
-, persistent, stdenv, text, fetchzip
+, persistent, stdenv, text, fetchFromGitHub, postgresql-simple, bytestring
 }:
 mkDerivation rec {
   pname = "flexible-instances";
-  version = "0.2.0";
-  src = fetchzip {
-    name = "${pname}-${version}.tar.gz";
-    url = "https://github.com/jb55/flexible-instances/archive/${version}.tar.gz";
-    sha256 = "1fm63bys1c9cbp8lgkrlyprksfhic05fw2hq9wh0axb5ggs1jyih";
+  version = "0.3.0";
+  src = fetchFromGitHub {
+    repo = "${pname}";
+    owner = "jb55";
+    sha256 = "1yffdlyhn7y2637r4j78niplz9b6rydf3y12hgzk6a98ap9fiqy7";
+    rev = version;
   };
   buildDepends = [
-    aeson base data-default flexible mtl persistent text
+    aeson base data-default flexible mtl persistent text postgresql-simple
+    bytestring
   ];
   description = "Helpful instances for Data.Flexible";
   license = stdenv.lib.licenses.mit;
