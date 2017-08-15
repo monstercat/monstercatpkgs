aa64fad42ce5042e27636454eacc819a0b61e76b
William Casarin <jb55@jb55.com>
2017-08-14 17:29:40 -0700

    update import-scripts

--- pkgs/import-scripts/default.nix
+++ pkgs/import-scripts/default.nix
@@ -1,6 +1,6 @@
 { fetchgit }:
 fetchgit {
   url = "http://git.monster.cat/import-scripts";
-  rev = "a939fbb6bb7f1c1a4f2b39c30f3e690ed30080fa";
-  sha256 = "1qh2fhjiikp7mlb4wj7pa6z13x59208ig0f5r4dgk8fn5ina02ss";
+  rev = "09a12b31c46c9b7f2f517c28577c86385cb09f0d";
+  sha256 = "05kj8b7xbkk1lm8wgb48mglik46wl6hfj1bvqv5g758g1nwzlnkx";
 }
