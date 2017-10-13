8ae510186210a2e2955fe3b5ca2ad63de87dbf18
William Casarin <jb55@jb55.com>
2017-10-13 10:51:14 -0700

    mcat-tools: bump?

--- pkgs/mcat-tools/default.nix
+++ pkgs/mcat-tools/default.nix
@@ -1,6 +1,6 @@
 { fetchgit }:
 fetchgit {
   url = "http://git.monster.cat/mcat-tools";
-  rev = "82bf173334dd339887903e9a564fc8ae3daddf29";
-  sha256 = "0mhzjs9zwd058c9dap08dm8ddk5x5g8gaxrw370kpkvz17h8jbyd";
+  rev = "10decab6e8768b360e09c1f9eeef2b876acb39fe";
+  sha256 = "1wwvf3gg8gffw6l5rfry9abl6n2k9ma3zmc4frng1b8bjcw7gxpc";
 }
