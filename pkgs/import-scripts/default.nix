fcc9d70fa6aa659eea3beb2337e73be4cfa72a62
William Casarin <jb55@jb55.com>
2017-11-17 10:48:52 -0800

    import-scripts: bump dat

--- pkgs/import-scripts/default.nix
+++ pkgs/import-scripts/default.nix
@@ -1,6 +1,6 @@
 { fetchgit }:
 fetchgit {
   url = "http://git.monster.cat/import-scripts";
-  rev = "1aa95f19d17a40889426aea13b97c093beac4c89" ;
-  sha256 = "08jnn6ihjfg6nj3y32pi4n78d1yqpk32hky0w15njbpjcrn5h8qy";
+  rev = "c8e72bfd98303b301394713d0d045233ee4c48e6" ;
+  sha256 = "1xvcls6n1vv5w3z55k2sy86dl2hcm5kdgr3kr7bhjh3yv5ly3aci";
 }
